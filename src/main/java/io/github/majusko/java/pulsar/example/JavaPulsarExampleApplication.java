a67c8c6a1d3f88a98611a2680878c614c6fdeb2e
www <www@qq.com>
2023-04-03 17:35:16 +0800

    test

--- src/main/java/io/github/majusko/java/pulsar/example/JavaPulsarExampleApplication.java
+++ src/main/java/io/github/majusko/java/pulsar/example/JavaPulsarExampleApplication.java
@@ -1,12 +1,80 @@
 package io.github.majusko.java.pulsar.example;
 
+import io.github.majusko.java.pulsar.example.configuration.Topics;
+import io.github.majusko.java.pulsar.example.producer.ProducerService;
+import io.github.majusko.pulsar.producer.PulsarTemplate;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.TypedMessageBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class JavaPulsarExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaPulsarExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(JavaPulsarExampleApplication.class, args);
+
+//        ProducerService producerService = applicationContext.getBean(ProducerService.class);
+//        send1(producerService);
+
+        PulsarTemplate<String> pulsarTemplate = applicationContext.getBean(PulsarTemplate.class);
+//        send2(pulsarTemplate);
+        send3(pulsarTemplate);
+    }
+
+    public static void send1(ProducerService producerService) {
+        try {
+            producerService.sendStringMsg();
+            producerService.sendClassMsg();
+        } catch (PulsarClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void send2(PulsarTemplate<String> pulsarTemplate) {
+        try {
+            // 同步发送消息
+            MessageId messageId = pulsarTemplate.send(Topics.STRING, "同步发送消息");
+
+            // 异步发送消息
+            CompletableFuture<MessageId> future = pulsarTemplate.sendAsync(Topics.STRING, "异步发送消息");
+            MessageId messageId1 = future.get();
+
+            // 自定义消息发送
+            String value = "自定义消息发送";
+            TypedMessageBuilder<String> builder = pulsarTemplate.createMessage(Topics.STRING, value);
+            builder.key("k1")
+                    .orderingKey("k1".getBytes())
+                    .property("n1", "v1")
+                    .deliverAfter(1, TimeUnit.MINUTES)
+                    .send();
+            System.out.println(String.format("发送消息: 内容[%s], 时间[%s]", value, new Date()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void send3(PulsarTemplate<String> pulsarTemplate) {
+        try {
+            // 批量发送消息，测试订阅类型
+            for (int i = 0; i < 20; i++) {
+                String key = "k" + i;
+                String value = "v" + i;
+                String orderingKey = "ok" + i;
+                TypedMessageBuilder<String> builder = pulsarTemplate.createMessage(Topics.STRING, value);
+                builder.key(key)
+                        .orderingKey(orderingKey.getBytes())
+                        .send();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/io/github/majusko/java/pulsar/example/consumer/ConsumerService.java
+++ src/main/java/io/github/majusko/java/pulsar/example/consumer/ConsumerService.java
@@ -3,8 +3,11 @@ package io.github.majusko.java.pulsar.example.consumer;
 import io.github.majusko.java.pulsar.example.configuration.Topics;
 import io.github.majusko.java.pulsar.example.data.MyMsg;
 import io.github.majusko.pulsar.annotation.PulsarConsumer;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
+import org.apache.pulsar.client.api.SubscriptionType;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
@@ -12,9 +15,10 @@ public class ConsumerService {
     public AtomicBoolean stringReceived = new AtomicBoolean(false);
     public AtomicBoolean classReceived = new AtomicBoolean(false);
 
-    @PulsarConsumer(topic = Topics.STRING, clazz = String.class)
+    @PulsarConsumer(topic = Topics.STRING, clazz = String.class, subscriptionType = SubscriptionType.Shared,
+            subscriptionName = "subscription1", consumerName = "consumeString1", initialPosition = SubscriptionInitialPosition.Latest)
     public void consumeString(String message) {
-        System.out.println(message);
+        System.out.println(String.format("接收消息: 内容[%s], 时间[%s]", message, new Date()));
         stringReceived.set(true);
     }
 
